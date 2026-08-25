38414e10743d62757e36a13474c3a5e7d6864955
Dominic Charl Mariano <charl1107@users.noreply.github.com>
2026-08-25 16:48:58 +0800

    Configure Vitest test runner and jsdom environment in package.json & vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
-  plugins: [vue(), tailwindcss()],
-  server: {
-    host: '0.0.0.0',
-  },
+plugins: [vue(), tailwindcss()],
+test: {
+environment: 'jsdom'
+}
 })
